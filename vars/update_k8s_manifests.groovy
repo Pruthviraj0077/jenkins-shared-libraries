b7f10ae88bf83d1a1f8883d303af213054166037
Pruthviraj0077 <76246590+Pruthviraj0077@users.noreply.github.com>
2025-07-08 11:03:33 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -41,7 +41,7 @@ def call(Map config = [:]) {
             # Check for changes
             if git diff --quiet; then
                 echo "No changes to commit"
-            else
+            #else
                 # Commit and push changes
                 git add ${manifestsPath}/*.yaml
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
